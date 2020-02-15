4f6b7a29a008a8ca7fc704b33e1b8abfe3ea69f2
Muhammad Reman <P2577087@my365.dmu.ac.uk>
2020-02-15 16:43:30 +0000

    Attempt 2

---
+++ Lab 3/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstData = new System.Windows.Forms.ListBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstData
+            // 
+            this.lstData.FormattingEnabled = true;
+            this.lstData.Location = new System.Drawing.Point(178, 45);
+            this.lstData.Name = "lstData";
+            this.lstData.Size = new System.Drawing.Size(284, 160);
+            this.lstData.TabIndex = 0;
+            this.lstData.SelectedIndexChanged += new System.EventHandler(this.lstData_SelectedIndexChanged);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(178, 235);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lstData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstData;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ Lab 3/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            List<string> Dates = new List<string>();
+            lstData.Items.Clear();
+
+            //creare the database string
+            string connectionString = Properties.Settings.Default.Data_set_0203_1_ConnectionString;
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbDataReader reader = null;
+                OleDbCommand getDates = new OleDbCommand("SELECT [Order Date], [Ship Date] from Sheet1", connection);
+
+                reader = getDates.ExecuteReader();
+                while (reader.Read())
+                {
+                    Dates.Add(reader[0].ToString());
+                    Dates.Add(reader[1].ToString());
+                }
+
+
+                lstData.DataSource = Dates;
+               
+            }
+        }
+
+        private void lstData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
